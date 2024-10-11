f217d9af6c892dd66ab4420e9a011526c58c0a78
Mohamed soliman <130843237+Mohammmedsoliman@users.noreply.github.com>
2024-10-11 20:57:18 +0300

    Add files via upload

---
+++ js/color-changer.js
@@ -0,0 +1,11 @@
+ function dark(){
+const element = document.getElementById('body');
+if (element) {
+  element.style.backgroundColor = 'black';
+  element.style.color = 'white';
+  }
+  else{
+    element.style.backgroundColor = 'black';
+    element.style.color = 'white';
+  }
+}
